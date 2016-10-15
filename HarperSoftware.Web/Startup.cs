8fabcd5e33f202241a031e751dcf2b98e4c4bec4
Richard <Richard@DESKTOP-K0E1O35>
2016-10-15 07:07:06 -0600

    Add MVC web site to solution.

---
+++ HarperSoftware.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HarperSoftware.Web.Startup))]
+namespace HarperSoftware.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
